d56798122d0b4df638772daba6524bb47ef2c954
Isaac Aires <playerredbrasil@gmail.com>
2025-03-19 21:51:26 -0300

    Refatoração da classe Character: reorganização dos atributos, simplificação dos construtores e implementação de getters e setters de forma mais concisa. Atributos de perícias foram mantidos, mas a estrutura do código foi melhorada para maior clareza e manutenção.

--- tome-of-heroes/src/main/java/com/tomeofheroes/tome_of_heroes/models/Character.java
+++ tome-of-heroes/src/main/java/com/tomeofheroes/tome_of_heroes/models/Character.java
@@ -19,7 +19,7 @@ public class Character {
     private int level;
     private int experience;
 
-    // Atributos movidos da classe Stats
+    // Atributos base
     private int strength;
     private int dexterity;
     private int constitution;
@@ -28,24 +28,24 @@ public class Character {
     private int charisma;
 
     // Perícias de D&D
-    private int acrobatics;       // Acrobacia (Destreza)
-    private int arcana;           // Arcanismo (Inteligência)
-    private int athletics;        // Atletismo (Força)
-    private int deception;        // Enganação (Carisma)
-    private int history;          // História (Inteligência)
-    private int insight;          // Intuição (Sabedoria)
-    private int intimidation;     // Intimidação (Carisma)
-    private int investigation;    // Investigação (Inteligência)
-    private int medicine;         // Medicina (Sabedoria)
-    private int nature;           // Natureza (Inteligência)
-    private int perception;       // Percepção (Sabedoria)
-    private int performance;      // Atuação (Carisma)
-    private int persuasion;       // Persuasão (Carisma)
-    private int religion;         // Religião (Inteligência)
-    private int sleightOfHand;    // Prestidigitação (Destreza)
-    private int stealth;          // Furtividade (Destreza)
-    private int survival;         // Sobrevivência (Sabedoria)
-    private int animalHandling;   // Adestramento (Sabedoria)
+    private int acrobatics;
+    private int arcana;
+    private int athletics;
+    private int deception;
+    private int history;
+    private int insight;
+    private int intimidation;
+    private int investigation;
+    private int medicine;
+    private int nature;
+    private int perception;
+    private int performance;
+    private int persuasion;
+    private int religion;
+    private int sleightOfHand;
+    private int stealth;
+    private int survival;
+    private int animalHandling;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
@@ -71,11 +71,8 @@ public class Character {
     )
     private Set<Spells> spells;
 
-    // Construtor padrão
-    public Character() {
-    }
+    public Character() {}
 
-    // Construtor com parâmetros
     public Character(UUID id, String name, int level, int experience, int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma) {
         this.id = id;
         this.name = name;
@@ -89,271 +86,77 @@ public class Character {
         this.charisma = charisma;
     }
 
-    // Getters e setters para os campos
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
-
-    public int getExperience() {
-        return experience;
-    }
-
-    public void setExperience(int experience) {
-        this.experience = experience;
-    }
-
-    public int getStrength() {
-        return strength;
-    }
-
-    public void setStrength(int strength) {
-        this.strength = strength;
-    }
-
-    public int getDexterity() {
-        return dexterity;
-    }
-
-    public void setDexterity(int dexterity) {
-        this.dexterity = dexterity;
-    }
-
-    public int getConstitution() {
-        return constitution;
-    }
-
-    public void setConstitution(int constitution) {
-        this.constitution = constitution;
-    }
-
-    public int getIntelligence() {
-        return intelligence;
-    }
-
-    public void setIntelligence(int intelligence) {
-        this.intelligence = intelligence;
-    }
-
-    public int getWisdom() {
-        return wisdom;
-    }
-
-    public void setWisdom(int wisdom) {
-        this.wisdom = wisdom;
-    }
-
-    public int getCharisma() {
-        return charisma;
-    }
-
-    public void setCharisma(int charisma) {
-        this.charisma = charisma;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Classe getClasse() {
-        return classe;
-    }
-
-    public void setClasse(Classe classe) {
-        this.classe = classe;
-    }
-
-    public Set<Inventory> getInventory() {
-        return inventory;
-    }
-
-    public void setInventory(Set<Inventory> inventory) {
-        this.inventory = inventory;
-    }
-
-    public Race getRace() {
-        return race;
-    }
-
-    public void setRace(Race race) {
-        this.race = race;
-    }
-
-    public Set<Spells> getSpells() {
-        return spells;
-    }
-
-    public void setSpells(Set<Spells> spells) {
-        this.spells = spells;
-    }
-
-    // Getters e setters para as perícias
-
-    public int getAcrobatics() {
-        return acrobatics;
-    }
-
-    public void setAcrobatics(int acrobatics) {
-        this.acrobatics = acrobatics;
-    }
-
-    public int getArcana() {
-        return arcana;
-    }
-
-    public void setArcana(int arcana) {
-        this.arcana = arcana;
-    }
-
-    public int getAthletics() {
-        return athletics;
-    }
-
-    public void setAthletics(int athletics) {
-        this.athletics = athletics;
-    }
-
-    public int getDeception() {
-        return deception;
-    }
-
-    public void setDeception(int deception) {
-        this.deception = deception;
-    }
-
-    public int getHistory() {
-        return history;
-    }
-
-    public void setHistory(int history) {
-        this.history = history;
-    }
-
-    public int getInsight() {
-        return insight;
-    }
-
-    public void setInsight(int insight) {
-        this.insight = insight;
-    }
-
-    public int getIntimidation() {
-        return intimidation;
-    }
-
-    public void setIntimidation(int intimidation) {
-        this.intimidation = intimidation;
-    }
-
-    public int getInvestigation() {
-        return investigation;
-    }
-
-    public void setInvestigation(int investigation) {
-        this.investigation = investigation;
-    }
-
-    public int getMedicine() {
-        return medicine;
-    }
-
-    public void setMedicine(int medicine) {
-        this.medicine = medicine;
-    }
-
-    public int getNature() {
-        return nature;
-    }
-
-    public void setNature(int nature) {
-        this.nature = nature;
-    }
-
-    public int getPerception() {
-        return perception;
-    }
-
-    public void setPerception(int perception) {
-        this.perception = perception;
-    }
-
-    public int getPerformance() {
-        return performance;
-    }
-
-    public void setPerformance(int performance) {
-        this.performance = performance;
-    }
-
-    public int getPersuasion() {
-        return persuasion;
-    }
-
-    public void setPersuasion(int persuasion) {
-        this.persuasion = persuasion;
-    }
-
-    public int getReligion() {
-        return religion;
-    }
-
-    public void setReligion(int religion) {
-        this.religion = religion;
-    }
-
-    public int getSleightOfHand() {
-        return sleightOfHand;
-    }
-
-    public void setSleightOfHand(int sleightOfHand) {
-        this.sleightOfHand = sleightOfHand;
-    }
-
-    public int getStealth() {
-        return stealth;
-    }
-
-    public void setStealth(int stealth) {
-        this.stealth = stealth;
-    }
-
-    public int getSurvival() {
-        return survival;
-    }
-
-    public void setSurvival(int survival) {
-        this.survival = survival;
-    }
-
-    public int getAnimalHandling() {
-        return animalHandling;
-    }
-
-    public void setAnimalHandling(int animalHandling) {
-        this.animalHandling = animalHandling;
-    }
+    // Getters e setters básicos
+    public UUID getId() { return id; }
+    public void setId(UUID id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public int getLevel() { return level; }
+    public void setLevel(int level) { this.level = level; }
+    public int getExperience() { return experience; }
+    public void setExperience(int experience) { this.experience = experience; }
+
+    // Getters e setters de atributos
+    public int getStrength() { return strength; }
+    public void setStrength(int strength) { this.strength = strength; }
+    public int getDexterity() { return dexterity; }
+    public void setDexterity(int dexterity) { this.dexterity = dexterity; }
+    public int getConstitution() { return constitution; }
+    public void setConstitution(int constitution) { this.constitution = constitution; }
+    public int getIntelligence() { return intelligence; }
+    public void setIntelligence(int intelligence) { this.intelligence = intelligence; }
+    public int getWisdom() { return wisdom; }
+    public void setWisdom(int wisdom) { this.wisdom = wisdom; }
+    public int getCharisma() { return charisma; }
+    public void setCharisma(int charisma) { this.charisma = charisma; }
+
+    // Getters e setters de relacionamentos
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+    public Classe getClasse() { return classe; }
+    public void setClasse(Classe classe) { this.classe = classe; }
+    public Set<Inventory> getInventory() { return inventory; }
+    public void setInventory(Set<Inventory> inventory) { this.inventory = inventory; }
+    public Race getRace() { return race; }
+    public void setRace(Race race) { this.race = race; }
+    public Set<Spells> getSpells() { return spells; }
+    public void setSpells(Set<Spells> spells) { this.spells = spells; }
+
+    // Getters e setters de perícias
+    public int getAcrobatics() { return acrobatics; }
+    public void setAcrobatics(int acrobatics) { this.acrobatics = acrobatics; }
+    public int getArcana() { return arcana; }
+    public void setArcana(int arcana) { this.arcana = arcana; }
+    public int getAthletics() { return athletics; }
+    public void setAthletics(int athletics) { this.athletics = athletics; }
+    public int getDeception() { return deception; }
+    public void setDeception(int deception) { this.deception = deception; }
+    public int getHistory() { return history; }
+    public void setHistory(int history) { this.history = history; }
+    public int getInsight() { return insight; }
+    public void setInsight(int insight) { this.insight = insight; }
+    public int getIntimidation() { return intimidation; }
+    public void setIntimidation(int intimidation) { this.intimidation = intimidation; }
+    public int getInvestigation() { return investigation; }
+    public void setInvestigation(int investigation) { this.investigation = investigation; }
+    public int getMedicine() { return medicine; }
+    public void setMedicine(int medicine) { this.medicine = medicine; }
+    public int getNature() { return nature; }
+    public void setNature(int nature) { this.nature = nature; }
+    public int getPerception() { return perception; }
+    public void setPerception(int perception) { this.perception = perception; }
+    public int getPerformance() { return performance; }
+    public void setPerformance(int performance) { this.performance = performance; }
+    public int getPersuasion() { return persuasion; }
+    public void setPersuasion(int persuasion) { this.persuasion = persuasion; }
+    public int getReligion() { return religion; }
+    public void setReligion(int religion) { this.religion = religion; }
+    public int getSleightOfHand() { return sleightOfHand; }
+    public void setSleightOfHand(int sleightOfHand) { this.sleightOfHand = sleightOfHand; }
+    public int getStealth() { return stealth; }
+    public void setStealth(int stealth) { this.stealth = stealth; }
+    public int getSurvival() { return survival; }
+    public void setSurvival(int survival) { this.survival = survival; }
+    public int getAnimalHandling() { return animalHandling; }
+    public void setAnimalHandling(int animalHandling) { this.animalHandling = animalHandling; }
 }
